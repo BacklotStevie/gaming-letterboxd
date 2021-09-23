de104e32e732899cdf28499b8c2142ff31814ded
Steven Sarria <ssarr012@fiu.edu>
2021-09-23 14:31:52 -0400

    created homepage design with Tailwind and JS

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import eldenring from '../Images/eldenring.jpeg'
+import nbacover from '../Images/2k22cover.jpeg'
+import fifacover from '../Images/fifa22cover.jpeg'
+import humankind from '../Images/humankindcover.jpeg'
+import kenacover from '../Images/kenacover.webp'
+import deathloop from '../Images/deathloopcover.jpeg'
 
 function Home(props) {
   //useState for json data file
@@ -19,11 +25,12 @@ function Home(props) {
   if (homeData) {
     var phrases = homeData.phrase.map((phrase) => {
       return (
-        <>
-          <div>{phrase.one}</div>
-          <div>{phrase.two}</div>
-          <div>{phrase.three}</div>
-        </>
+        <div class='flex-row bg-gradient-to-b from-yellow-300 to-gray-800'>
+          <img src={eldenring} alt='...' class='mx-auto w-2/3 pt-3'></img>
+          <div class='text-white italic font-sans text-xl font-bold pt-5'>{phrase.one}</div>
+          <div class='text-white italic font-sans text-xl font-bold pt-1'>{phrase.two}</div>
+          <div class='text-white italic font-sans text-xl font-bold pt-1 pb-5'>{phrase.three}</div>
+        </div>
       );
     });
     var socialMessage = homeData.socialMessage;
@@ -33,11 +40,18 @@ function Home(props) {
   return (
     <>
       <div>{phrases}</div>
-      <div className='border-2'>
-        <button>JOIN NOW - IT'S FREE</button>
+      <div className='pt-7 pb-7 bg-gradient-to-b from-gray-800 to-gray-800'>
+        <button class="shadow bg-yellow-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">JOIN NOW - IT'S FREE</button>
       </div>
-      <div>{socialMessage}</div>
-      <div>{reviewMessage}</div>
+      <div class='text-white italic pt-6 pb-6 font-sans text-xl font-bold pt-5 bg-gradient-to-b from-gray-800 to-gray-800'>{socialMessage}</div>
+      <div class='flex justify-center bg-gradient-to-b from-gray-800 to-gray-800'>
+        <img src={nbacover} alt='...' class='pt-3 px-4 h-60'></img>
+        <img src={fifacover} alt='...' class='pt-3 px-4 h-60'></img>
+        <img src={humankind} alt='...' class='pt-3 px-4 h-60'></img>
+        <img src={kenacover} alt='...' class='pt-3 px-4 h-60'></img>
+        <img src={deathloop} alt='...' class='pt-3 px-4 h-60'></img>
+      </div>
+      <div class='text-white italic pt-6 pb-6 font-sans text-xl font-bold pt-5 bg-gradient-to-b from-gray-800 to-gray-800'>{reviewMessage}</div>
     </>
   );
 }
--- src/Components/header.js
+++ src/Components/header.js
@@ -3,22 +3,18 @@ import * as React from "react"
 const Header = ({ siteTitle }) => {
   return (
     <>
-      <nav class="justify-between flex-wrap bg-teal p-5">
+      <nav class="justify-between flex-wrap bg-teal p-5 bg-gradient-to-b from-yellow-300 to-yellow-300">
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-lg place-items: center lg:flex-grow text-center">
-            <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700 mr-7">
+            <a href="" class="text-white font-extrabold block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700 mr-7">
               Login/User
             </a>
-            <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700 mr-7">
+            <a href="" class="text-white font-extrabold block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700 mr-7">
               Activity
             </a>
-            <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700">
-               Other
+            <a href="" class="text-white font-extrabold block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700">
+               Games
             </a>
-            <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black text-decoration: underline hover:no-underline hover:text-red-700 mr-7">
-              List
-            </a>
-
         </div>
       </div>
     </nav>
